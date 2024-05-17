c681673b81ee1f92c0a7fa5ed123b1ec9fb5df91
Drew Heasman <drew.heasman@avos.io>
2024-05-17 15:31:28 +0100

    Collate functions, collect path definitions

---
+++ sentry_api/path/projects_path.py
@@ -0,0 +1,3 @@
+def project_issues_path(organization_slug, project_slug: str):
+    return f"projects/{organization_slug}/{project_slug}/issues/"
+
--- sentry_api/resources/issue.py
+++ sentry_api/resources/issue.py
@@ -1,5 +1,7 @@
 from urllib.parse import quote_plus, urljoin
 
+from sentry_api.path.projects_path import project_issues_path
+from sentry_api.path.organizations_path import org_issues_events_path, org_issues_tags_path, org_issues_tags_values_path
 from sentry_api.resources.pagination import get_all_pages
 from .base import BaseResource
 
@@ -7,57 +9,37 @@ class IssuesResource(BaseResource):
     """
     Sentry API: https://docs.sentry.io/api/events/
     """
-    org_slug_path = "organizations/{organization_slug}/"
-    def __project_issues_path(self, project_slug: str):
-        return f"projects/{self.organization_slug}/{project_slug}/issues/"
-    def __org_issues_events_path(self, issue_id: str):
-        return f"organizations/{self.organization_slug}/issues/{issue_id}/events/"
-    def __org_issues_tags_path(self, issue_id: str, key: str):
-        return f"organizations/{self.organization_slug}/issues/{issue_id}/tags/{key}/"
-    def __org_issues_tags_values_path(self, issue_id: str, key: str):
-        return f"{self.__org_issues_tags_path(issue_id, key)}values/"
 
-    def list(self, project_slug: str):
+    def list(self, project_slug: str, all = False):
         """
         Sentry API: https://docs.sentry.io/api/events/list-a-projects-issues/
         """
-        return self.http_client.make_a_call("get", self.__project_issues_path(project_slug))
+        if all:
+            return get_all_pages(self, urljoin(self.http_client.endpoint_url, project_issues_path(self.organization_slug, project_slug)))
+        return self.http_client.make_a_call("get", project_issues_path(self.organization_slug, project_slug))
 
-    def list_all(self, project_slug: str):
-        """
-        Page through all issues in a project and return list of issues
-        """
-        return get_all_pages(self, urljoin(self.http_client.endpoint_url, self.__project_issues_path(project_slug)))
-
-    def get_events(self, issue_id: str):
+    def get_events(self, issue_id: str, all = False):
         """
         Sentry API: https://docs.sentry.io/api/events/list-an-issues-events/
         """
-        return self.http_client.make_a_call("get", self._issue_events_path(issue_id))
+        if all:
+            return get_all_pages(self, urljoin(self.http_client.endpoint_url, org_issues_events_path(self.organization_slug, issue_id)))
+        return self.http_client.make_a_call("get", org_issues_events_path(self.organization_slug, issue_id))
 
     def get_events_query(self, issue_id: str, query: str):
-        return self.http_client.make_a_call("get", urljoin(self.__org_issues_events_path(issue_id),f'?query={quote_plus(query)}'))
-
-    def get_events_all(self, issue_id: str):
-        """
-        Page through all events for an issue and return list of events
-        """
-        return get_all_pages(self, urljoin(self.http_client.endpoint_url, self.__org_issues_events_path(issue_id)))
+        return self.http_client.make_a_call("get", urljoin(org_issues_events_path(self.organization_slug, issue_id),f'?query={quote_plus(query)}'))
 
     def get_tag_details(self, issue_id: str, key: str):
         """
         Sentry API: https://docs.sentry.io/api/events/retrieve-tag-details/
         """
-        return self.http_client.make_a_call("get", self.__org_issues_tags_path(issue_id, key))
+        return self.http_client.make_a_call("get", org_issues_tags_path(self.organization_slug, issue_id, key))
 
-    def get_tag_values(self, issue_id: str, key: str):
+    def get_tag_values(self, issue_id: str, key: str, all = False):
         """
         Sentry API: https://docs.sentry.io/api/events/list-a-tags-values-related-to-an-issue/
         """
-        return self.http_client.make_a_call("get", self.__org_issues_tags_values_path(issue_id, key))
+        if all:
+            return get_all_pages(self, urljoin(self.http_client.endpoint_url, org_issues_tags_values_path(self.organization_slug, issue_id, key)))
+        return self.http_client.make_a_call("get", org_issues_tags_values_path(self.organization_slug, issue_id, key))
 
-    def get_tag_values_all(self, issue_id: str, key: str):
-        """
-        Sentry API: https://docs.sentry.io/api/events/list-an-issues-events/
-        """
-        return get_all_pages(self, urljoin(self.http_client.endpoint_url, self.__org_issues_tags_values_path(issue_id, key)))
